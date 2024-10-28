8ed9202035abdfed1b174f784222e94ca5586533
chaeyoungeee <chaeinge@gmail.com>
2024-10-28 12:51:18 +0900

    Add Swagger configuration

--- src/main/java/com/team4/goorm/community/TestController.java
+++ src/main/java/com/team4/goorm/community/TestController.java
@@ -1,16 +1,25 @@
 package com.team4.goorm.community;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 
+@Tag(name = "Test Controller", description = "테스트 API")
 @Slf4j
 @RestController
 public class TestController {
 
+	@Operation(summary = "테스트")
 	@GetMapping("/test")
-	public String test() {
-		return "test";
+	public String test(
+		@Parameter(description = "메시지", example = "테스트입니다.")
+		@RequestParam("msg") String msg) {
+		return msg;
 	}
 }
---
+++ src/main/java/com/team4/goorm/community/global/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.team4.goorm.community.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.extern.slf4j.Slf4j;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Community API",
+                version = "v1.0.0",
+                description = "Goorm Team4 Community API 명세서입니다."
+        )
+)
+@Configuration
+@Slf4j
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .components(new Components());
+    }
+}

9c6ed7deb86c94e1cc87a1f360e851ed857261c5
chaeyoungeee <chaeinge@gmail.com>
2024-10-29 17:40:43 +0900

    Feat(#1): member entity, repository 생성

--- src/main/java/com/team4/goorm/community/Member/domain/Member.java
+++ src/main/java/com/team4/goorm/community/Member/domain/Member.java
@@ -2,10 +2,13 @@ package com.team4.goorm.community.Member.domain;
 
 import com.team4.goorm.community.global.common.domain.BaseEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,5 +20,21 @@ public class Member extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long memberId;
+
+	@Email
+	@Column(nullable = false)
+	private String email;
+
+	@Size(min = 6, max = 20)
+	@Column(nullable = false)
+	private String loginId;
+
+	@Size(min = 2, max = 20)
+	@Column(nullable = false)
+	private String username;
+
+	@Size(min = 8, max = 20)
+	@Column(nullable = false)
+	private String password;
 }

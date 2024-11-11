38c1903f1063e6808f6ca630a7d34f7613da6f35
chaeyounglim <limcy423@gmail.com>
2024-11-11 20:28:08 +0900

    Refactor: 메일 생성 응답 타입 SimpleMailMessage -> MimeMessage로 변경
    html 형식으로 메일을 보내기 위해 변경함

--- src/main/java/com/team4/goorm/community/mail/application/MailService.java
+++ src/main/java/com/team4/goorm/community/mail/application/MailService.java
@@ -2,10 +2,12 @@ package com.team4.goorm.community.mail.application;
 
 import com.team4.goorm.community.mail.exception.MailErrorCode;
 import com.team4.goorm.community.mail.exception.MailException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,19 +18,20 @@ public class MailService {
     private final JavaMailSender mailSender;
 
     public void sendEmail(String toEmail, String title, String text) {
-        SimpleMailMessage email = createEmail(toEmail, title, text);
         try {
+            MimeMessage email = createEmail(toEmail, title, text);
             mailSender.send(email);
-        } catch (RuntimeException e) {
+        } catch (MessagingException e) {
             throw new MailException(MailErrorCode.MAIL_ERROR);
         }
     }
 
-    private SimpleMailMessage createEmail(String toEmail, String title, String text) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject(title);
-        message.setText(text);
+    private MimeMessage createEmail(String toEmail, String subject, String text) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
+        helper.setTo(toEmail);
+        helper.setSubject(subject);
+        helper.setText(text, true); // html 형식 활성화
 
         return message;
     }

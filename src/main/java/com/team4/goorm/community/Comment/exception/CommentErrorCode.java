4e7a0ae5c0e0c38db344959eb15b3672147d0e85
chaeyounglim <limcy423@gmail.com>
2024-11-15 16:33:54 +0900

    Fix: exception 버그 수정

--- src/main/java/com/team4/goorm/community/Comment/exception/CommentErrorCode.java
+++ src/main/java/com/team4/goorm/community/Comment/exception/CommentErrorCode.java
@@ -14,7 +14,7 @@ public enum CommentErrorCode implements BaseErrorCode{
         COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT_404_0", "존재하지 않는 댓글입니다."),
         COMMENT_NOT_AUTHORIZED(HttpStatus.FORBIDDEN, "COMMENT_403_0", "권한이 없습니다.");
         
-        private final HttpStatus httpStatus;
+        private final HttpStatus status;
         private final String code;
         private final String message;
 }

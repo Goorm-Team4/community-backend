f72d1a9bab5c7c8379ee3760a0a13d5e9e89acf1
chaeyounglim <limcy423@gmail.com>
2024-12-03 20:47:47 +0900

    Fix: 비밀번호 변경 시 암호화 추가

--- src/main/java/com/team4/goorm/community/auth/exception/AuthErrorCode.java
+++ src/main/java/com/team4/goorm/community/auth/exception/AuthErrorCode.java
@@ -13,6 +13,7 @@ public enum AuthErrorCode implements BaseErrorCode {
 
 	EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "AUTH_400_0", "이미 가입된 이메일입니다."),
 	USERNAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "AUTH_400_1", "이미 사용중인 닉네임입니다."),
+	PASSWORD_NOT_CHANGED(HttpStatus.BAD_REQUEST, "AUTH_400_2", "이미 사용중인 비밀번호입니다."),
 	INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "AUTH_401_0", "잘못된 이메일 또는 비밀번호입니다."),
 	;
 
